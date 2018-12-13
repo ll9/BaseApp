6eaa7e33cb8c10c5fac37d8ddeaf56e286f58548
ll9 <llwierer@web.de>
2018-12-13 21:31:08 +0100

    Init class library reference

--- BaseApp/Form1.cs
+++ BaseApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,8 @@ namespace BaseApp
         public Form1()
         {
             InitializeComponent();
+            var lp = new Lichtpunkt();
+            var mast = new Mast();
         }
     }
 }
